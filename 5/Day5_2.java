1bedbb24f6ed177eb0e370022749189ce6f5e3a5
Janszczyrek <janwalus@gmail.com>
2022-12-05 12:28:37 +0100

    day 5

---
+++ 5/Day5_2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.lang.Character;
+
+public class Day5_2 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader("input"));
+        String line;
+        StackList stacks = new StackList();
+        Stack<Character> temp = new Stack<>();
+        while ((line = input.readLine()) != null) {
+            if(line.contains("move")) {
+                int count = Integer.valueOf(line.split(" ")[1]);
+                int from = Integer.valueOf(line.split(" ")[3]);
+                int to = Integer.valueOf(line.split(" ")[5]);
+                for(int i = 0; i < count; i++) {
+                    temp.push(stacks.stacklist.get(from-1).pop());
+                }
+                while(!temp.empty()) {
+                    stacks.stacklist.get(to-1).push(temp.pop());
+                }
+            }
+        }
+        input.close();
+        System.out.println(stacks.topElements());
+    }
+
+}
+
+class StackList {
+    public ArrayList<Stack<Character>> stacklist;
+    StackList(){
+        stacklist = new ArrayList<>();
+        populate();
+    }
+    private void populate() {
+        for(int i = 0;i < stack_data.length; i++)
+        {
+            stacklist.add(new Stack<Character>());
+            for(char c : stack_data[i].toCharArray()) {
+               stacklist.get(i).push(Character.valueOf(c));
+        }
+
+        }
+    }
+    public String topElements() {
+        String res = "";
+        for(Stack<Character> s : stacklist) {
+            res += s.peek();
+        }
+        return res;
+    }
+    private String[] stack_data = {"WRF","THMCDVWP","PMZNL","JCHR","CPGHQTB","GCWLFZ","WVLQZJGC","PNRFWTVC","JWHGRSV"};
+}
